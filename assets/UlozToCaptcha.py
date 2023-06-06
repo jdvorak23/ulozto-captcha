437c61c1a62ba8afab983c954f7582d41970fe72
Jdvorak23 <dvorakj3@post.cz>
2023-06-06 15:13:18 +0200

    hook

--- assets/UlozToCaptcha.py
+++ assets/UlozToCaptcha.py
@@ -9,11 +9,12 @@ from ..internal.Addon import Addon
 class UlozToCaptcha(Addon):
     __name__ = "UlozToCaptcha"
     __type__ = "hook"
-    __version__ = "0.1"
+    __version__ = "0.2"
     __status__ = "testing"
 
     __config__ = [("activated", "bool", "Activated", True),
-                  ("folder", "string", "Directory with captcha resolver", "/root/ulozto-captcha/")]
+                  ("folder", "string", "Directory with captcha resolver", "/root/ulozto-captcha/"),
+                  ("python3", "string", "Exact python3 interpreter which has all dependencies installed", "/root/.pyenv/versions/3.9.16/bin/python")]
 
     __description__ = """Solve captcha by tensor"""
     __license__ = "GPLv3"
@@ -27,17 +28,18 @@ class UlozToCaptcha(Addon):
         task.handler.append(self)
         task.data['service'] = self.classname
         task.setWaiting(100)
+
         image = task.captchaParams['file']
-        
         with open(self.config.get('folder') + "captcha.txt", "w") as text_file:
-            text_file.write("/root/.pyload/" + image)
-        
-        proc = subprocess.Popen("python3 " + self.config.get('folder') + "captcha.py", shell=True, stdout=subprocess.PIPE)
-        result = proc.communicate()[0].rstrip()  
+            text_file.write(os.getcwd() + "/" + image)
+
+        self.log_info(self.config.get('folder') + "captcha.py")
+        proc = subprocess.Popen(self.config.get('python3') + " " + self.config.get('folder') + "captcha.py", shell=True, stdout=subprocess.PIPE)
+        result = proc.communicate()[0].rstrip()
         self.log_info("Captcha result: " + result)
         task.data['captchaResult'] = result
         task.setResult(result)
-        
+
     def captcha_invalid(self, task):
         self.log_info("Wrong captcha");
         if task.data['service'] == self.classname and "captchaResult" in task.data:
@@ -47,5 +49,6 @@ class UlozToCaptcha(Addon):
                 self.log_info("Wrong captcha image removed: " + file);
             except OSError:
                 pass
+
 
 
