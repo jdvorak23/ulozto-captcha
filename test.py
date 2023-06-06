10fa4a4e76e89cd6a32ef15241b3471291b2e0e1
Jdvorak23 <dvorakj3@post.cz>
2023-06-06 17:01:36 +0200

    no txt

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import subprocess
 
 env = {'IMAGE_PATH': 'images/good.jpg'}
 
-proc = subprocess.Popen("python3 captcha.py", shell=True, stdout=subprocess.PIPE, env=env)
+proc = subprocess.Popen("/root/.pyenv/versions/3.9.16/bin/python captcha.py", shell=True, stdout=subprocess.PIPE, env=env)
 result = proc.communicate()[0].rstrip()
 
 print(result)
